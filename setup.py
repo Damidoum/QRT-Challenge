61c27d1ab91f4f136be60a9e264d2ea2abf946d5
Damien Rouchouse <damien.rouchouse@etu.minesparis.psl.eu>
2023-11-20 18:00:22 +0100

    initi of the project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='We try to predict the price of the electricity in France and Germany.',
+    author='Damien Rouchouse',
+    license='',
+)
